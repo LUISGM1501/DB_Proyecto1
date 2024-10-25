ca3938ca793bb0ef8cae6e84047316a80e518d39
mora-urb <andresmora1914@gmail.com>
2024-10-24 18:33:41 -0600

    Update test_comment_routes.py

--- test/integracion/test_comment_routes.py
+++ test/integracion/test_comment_routes.py
@@ -1,9 +1,8 @@
 import pytest
-from flask import Flask, jsonify
+from unittest.mock import patch
 from flask_jwt_extended import create_access_token
 from app import app
-from controllers import comment_controller
-from unittest.mock import patch
+from unittest.mock import Mock
 
 @pytest.fixture
 def client():
@@ -13,73 +12,112 @@ def client():
         with app.app_context():
             yield client
 
-# Prueba de integración para crear un comentario
-@patch('controllers.comment_controller.create_comment')
-def test_create_comment(mock_create_comment, client):
-    mock_create_comment.return_value = 1
+@patch('controllers.follow_controller.follow_user')
+def test_follow_user(mock_follow_user, client):
+    mock_follow_user.return_value = 1 
 
     access_token = create_access_token(identity=1)
 
-    data = {
-        'user_id': 1,
-        'content': 'Test comment',
-        'post_id': 1
-    }
-
     response = client.post(
-        '/comments',
-        json=data,
+        '/follow/2',  
         headers={'Authorization': f'Bearer {access_token}'}
     )
 
     assert response.status_code == 201
-    assert response.json == {"message": "Comment created successfully", "comment_id": 1}
-    mock_create_comment.assert_called_once_with(1, 'Test comment', 1, None)
+    assert response.json == {"message": "User followed successfully", "follow_id": 1}
+    mock_follow_user.assert_called_once_with(1, 2)
 
-# Prueba de integración para obtener comentarios
-@patch('controllers.comment_controller.get_comments')
-def test_get_comments(mock_get_comments, client):
-    mock_get_comments.return_value = [
-        {
-            'id': 1,
-            'user_id': 1,
-            'content': 'Test comment',
-            'post_id': 1,
-            'place_id': None
-        },
-        {
-            'id': 2,
-            'user_id': 2,
-            'content': 'Another comment',
-            'post_id': 1,
-            'place_id': None
-        }
-    ]
+@patch('controllers.follow_controller.unfollow_user')
+def test_unfollow_user(mock_unfollow_user, client):
+    mock_unfollow_user.return_value = 1  
+
+    access_token = create_access_token(identity=1)
+
+    response = client.post(
+        '/unfollow/2',
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200
+    assert response.json == {"message": "User unfollowed successfully", "follow_id": 1}
+    mock_unfollow_user.assert_called_once_with(1, 2)
+
+@patch('controllers.follow_controller.get_followed_users')
+def test_get_followed_users(mock_get_followed_users, client):
+    user_mock_1 = Mock()
+    user_mock_1.to_dict.return_value = {
+        "id": 2,
+        "username": "user2",
+        "email": "user2@example.com"
+    }
+
+    user_mock_2 = Mock()
+    user_mock_2.to_dict.return_value = {
+        "id": 3,
+        "username": "user3",
+        "email": "user3@example.com"
+    }
+
+    mock_get_followed_users.return_value = [user_mock_1, user_mock_2]
 
     access_token = create_access_token(identity=1)
 
     response = client.get(
-        '/comments',
-        headers={'Authorization': f'Bearer {access_token}'},
-        query_string={'post_id': 1}
+        '/following',
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200, f"Unexpected error: {response.get_json()}"
+    assert len(response.json) == 2
+    assert response.json[0]['username'] == 'user2'
+    mock_get_followed_users.assert_called_once_with(1)
+
+@patch('controllers.follow_controller.get_followers')
+def test_get_followers(mock_get_followers, client):
+    user_mock_1 = Mock()
+    user_mock_1.to_dict.return_value = {
+        "id": 2,
+        "username": "user2",
+        "email": "user2@example.com"
+    }
+
+    user_mock_2 = Mock()
+    user_mock_2.to_dict.return_value = {
+        "id": 4,
+        "username": "user4",
+        "email": "user4@example.com"
+    }
+
+    mock_get_followers.return_value = [user_mock_1, user_mock_2]
+
+    access_token = create_access_token(identity=1)
+
+    response = client.get(
+        '/followers',
+        headers={'Authorization': f'Bearer {access_token}'}
+    )
+
+    assert response.status_code == 200, f"Unexpected error: {response.get_json()}"
+    assert len(response.json) == 2
+    assert response.json[0]['username'] == 'user2'
+    mock_get_followers.assert_called_once_with(1)
+
+@patch('controllers.follow_controller.get_feed')
+def test_get_feed(mock_get_feed, client):
+    
+    mock_get_feed.return_value = [
+        {"id": 1, "content": "Test post 1", "user_id": 2},
+        {"id": 2, "content": "Test post 2", "user_id": 3}
+    ]
+    
+    access_token = create_access_token(identity=1)
+
+    response = client.get(
+        '/feed?page=1&page_size=10',
+        headers={'Authorization': f'Bearer {access_token}'}
     )
 
     assert response.status_code == 200
-    assert response.json == [
-        {
-            'id': 1,
-            'user_id': 1,
-            'content': 'Test comment',
-            'post_id': 1,
-            'place_id': None
-        },
-        {
-            'id': 2,
-            'user_id': 2,
-            'content': 'Another comment',
-            'post_id': 1,
-            'place_id': None
-        }
-    ]
-    
-    mock_get_comments.assert_called_once_with("1", None)
+    assert len(response.json) == 2
+    assert response.json[0]['content'] == 'Test post 1'
+    mock_get_feed.assert_called_once_with(1, 1, 10)
